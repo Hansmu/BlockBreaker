22d259602c6fd1d988e759f9a9413bc20c59c010
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-07-26 20:21:37 +0300

    Added music.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.DontDestroyOnLoad(gameObject); //gameObject is the game object attached to the script.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
