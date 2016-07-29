25fcad1572fbaf581442e6b8c7c72318b810568a
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-07-29 14:10:32 +0300

    Added trigger event handler and notes about it. Lose screen redirect on trigger.

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {		
+	/**They work very much like start and update.**/
+	/**on collision - void OnCollisionEnter2D(Collision2D collision)**/
+	/**on trigger - void OnTriggerEnter2D(Collider2D collider)**/
+
+	//Expose the LevelManager
+	//Connect it via Unity by dragging and dropping.
+	//Adding it here will create a new field in Unity's script.
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		print("Trigger");
+		levelManager.LoadLevel ("Win");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		print("Collision");
+	}
+}
