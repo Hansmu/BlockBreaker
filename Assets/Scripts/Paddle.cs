17bf9827c307af33a6f48b8a6d4a440d9bb91a96
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-07-31 15:57:02 +0300

    Modfied sound levels. Modified sounds. Added autoplay. Modified levels.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
+	public bool autoPlay = false;
+
 	private const int WORLD_UNITS = 16;
 
-	// Use this for initialization
-	void Start () {
-	
+	private Ball ball;
+
+	void Start() {
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
+		if (!autoPlay) {
+			MoveWithMouse ();
+		} else {
+			AutoPlay();
+		}
+	}
+
+	void MoveWithMouse() {
 		float mousePositionInBlocks = Input.mousePosition.x / Screen.width * WORLD_UNITS;
 
 		//This refers to the script that is connected with the object.
@@ -21,6 +31,13 @@ public class Paddle : MonoBehaviour {
 			this.transform.position.z
 		);
 
+		this.transform.position = paddlePosition;
+	}
+
+	void AutoPlay() {
+		Vector3 paddlePosition = new Vector3(0.5f, this.transform.position.y, 0f);
+		Vector3 ballPosition = ball.transform.position;
+		paddlePosition.x = Mathf.Clamp(ballPosition.x, 0.5f, 15.5f);
 		this.transform.position = paddlePosition;
 	}
 }
